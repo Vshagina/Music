0627606e9514f3edd05ae82006f9d7cc508ffbd9
Victoria <123573080+Vshagina@users.noreply.github.com>
2023-11-02 01:27:15 +0300

    playback music

--- Main.java
+++ Main.java
@@ -1,101 +1,93 @@
 package com.company;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+import java.io.*;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
-import javax.sound.sampled.*;
-
 
 public class Main {
 
     public static void main(String[] args) {
-        String inputFilePath = "C:\\Users\\shagi\\Desktop\\music\\inFile.txt";
-        String outputFilePath = "C:\\Users\\shagi\\Desktop\\music\\outFile.txt";
+        String musicInputFilePath = "C:\\Users\\shagi\\Desktop\\music\\inFile.txt";
+        String musicOutputFilePath = "C:\\Users\\shagi\\Desktop\\music\\outFile.txt";
 
-        try {
-            List<String> musicURLs = readURLsFromFile(inputFilePath);
-            writeURLsToFile(outputFilePath, musicURLs);
+        List<String> musicURLs = readURLsFromFile(musicInputFilePath);
+        writeURLsToFile(musicOutputFilePath, musicURLs);
+
+        Thread musicThread = new Thread(() -> {
             downloadMusicFiles(musicURLs);
             playMusicFiles(musicURLs);
-        } catch (IOException e) {
+        });
+
+        musicThread.start();
+
+        try {
+            musicThread.join();
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-            System.err.println("Неподдерживаемый формат аудиофайла: " + e.getMessage());
-        } catch (LineUnavailableException e) {
-            System.err.println("Линия недоступна: " + e.getMessage());
         }
     }
 
-    private static List<String> readURLsFromFile(String filePath) throws IOException {
-        List<String> musicURLs = new ArrayList<>();
+    private static List<String> readURLsFromFile(String filePath) {
+        List<String> URLs = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                musicURLs.add(line);
+                URLs.add(line);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return musicURLs;
+        return URLs;
     }
 
-    private static void writeURLsToFile(String filePath, List<String> musicURLs) throws IOException {
+    private static void writeURLsToFile(String filePath, List<String> URLs) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            for (String url : musicURLs) {
+            for (String url : URLs) {
                 writer.write(url);
                 writer.newLine();
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    private static void downloadMusicFiles(List<String> musicURLs) throws IOException {
+    private static void downloadMusicFiles(List<String> musicURLs) {
         String downloadDirectory = "C:\\Users\\shagi\\Desktop\\music\\music";
         for (String url : musicURLs) {
             String fileName = getFileNameFromURL(url);
-            URL downloadURL = new URL(url);
-            Path targetPath = new File(downloadDirectory, fileName).toPath();
-            Files.copy(downloadURL.openStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+            URL downloadURL = null;
+            try {
+                downloadURL = new URL(url);
+                Path targetPath = new File(downloadDirectory, fileName).toPath();
+                Files.copy(downloadURL.openStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private static String getFileNameFromURL(String url) {
         int lastSlashIndex = url.lastIndexOf('/');
         String fileName = url.substring(lastSlashIndex + 1);
-        return fileName.endsWith(".mp3") ? fileName : fileName + ".mp3";
+        return fileName;
     }
 
-    private static void playMusicFiles(List<String> musicURLs) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+    private static void playMusicFiles(List<String> musicURLs) {
         for (String url : musicURLs) {
             String fileName = getFileNameFromURL(url);
-            String filePath = "C:\\Users\\shagi\\Desktop\\music\\music" + File.separator + fileName;
+            String filePath = "C:\\Users\\shagi\\Desktop\\music\\music\\" + fileName;
 
-            File audioFile = new File(filePath);
-            try {
-                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-                AudioFormat format = audioStream.getFormat();
-                DataLine.Info info = new DataLine.Info(Clip.class, format);
-                Clip audioClip = (Clip) AudioSystem.getLine(info);
-                audioClip.open(audioStream);
-                audioClip.start();
-
-                while (audioClip.isOpen()) {
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (UnsupportedAudioFileException e) {
-                System.err.println("Неподдерживаемый формат аудио файла для: " + fileName);
+            try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+                AdvancedPlayer player = new AdvancedPlayer(fileInputStream);
+                player.play();
+            } catch (IOException | JavaLayerException e) {
+                e.printStackTrace();
             }
         }
     }
